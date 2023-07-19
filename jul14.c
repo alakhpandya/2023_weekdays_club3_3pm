f413c6885ae74fb4df446ed5dcc0d9f1d46dd58e
alakhpandya <alakhpandya@gmail.com>
2023-07-19 12:59:45 +0530

    Create jul14.c

---
+++ jul14.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+    // Ask integer values for two 3x3 matrices, store them and store their addition in another 3x3 matrix. Print that matrix.
+    // Ask integer values for two 3x3 matrices, store them and store their multiplication in another 3x3 matrix. Print that matrix.
+    return 0;
+}
